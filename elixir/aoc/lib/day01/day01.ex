ec52ceff859805f0b5bbd729fe1cadd09ac50bd3
Adrian Cruz <drincruz@gmail.com>
2019-12-01 01:44:41 -0500

    [Day01] The Tyranny of the Rocket Equation Part 2
    Find the total fuel requirements considering that additional fuel will require additional fuel!

--- elixir/aoc/lib/day01/day01.ex
+++ elixir/aoc/lib/day01/day01.ex
@@ -6,6 +6,9 @@ defmodule Aoc.Day01 do
 
   Part 1
     * Find the sum of the fuel requirements.
+  Part 2
+    * Find the sum of the fuel requirements, but consider that additional fuel
+    requires more fuel.
   """
   @day01 Path.join(["lib/day01/data.txt"])
 
@@ -31,15 +34,44 @@ defmodule Aoc.Day01 do
   end
 
   @doc """
+  Considering additional fuel will need more fuel, calculate total fuel.
+
+  This function recursively gets a list of fuel totals using calculate_fuel/1
+  until the mass of required additional fuel is 0.
+  """
+  def calculate_total_fuel(mass), do: calculate_total_fuel(mass, [])
+  def calculate_total_fuel(mass, total_fuel) when mass < 3, do: Enum.sum(total_fuel)
+
+  def calculate_total_fuel(mass, total_fuel) do
+    case calculate_fuel(mass) do
+      fuel when fuel < 1 -> calculate_total_fuel(0, total_fuel)
+      fuel -> calculate_total_fuel(fuel, [fuel | total_fuel])
+    end
+  end
+
+  @doc """
   This will bind all of our functions together and calculate the fuel requirements.
 
    - Read the file
-   - Calculate the fueld requirements per mass
+   - Calculate the fuel requirements per mass
    - Sum the updated data
   """
   def fuel_requirements do
     read()
     |> Stream.map(&calculate_fuel/1)
+    |> Enum.sum()
+  end
+
+  @doc """
+  This will bind all of our functions together and calculate the fuel requirements.
+
+   - Read the file
+   - Calculate the total fuel requirements per mass
+   - Sum the updated data
+  """
+  def total_fuel_requirements do
+    read()
+    |> Stream.map(&calculate_total_fuel/1)
     |> Enum.sum()
   end
 end
--- elixir/aoc/test/day01/day01_test.exs
+++ elixir/aoc/test/day01/day01_test.exs
@@ -8,4 +8,10 @@ defmodule Aoc.Day01Test do
     assert Day01.calculate_fuel(1_969) == 654
     assert Day01.calculate_fuel(100_756) == 33_583
   end
+
+  test "calculates total fuel" do
+    assert Day01.calculate_total_fuel(14) == 2
+    assert Day01.calculate_total_fuel(1_969) == 966
+    assert Day01.calculate_total_fuel(100_756) == 50_346
+  end
 end
