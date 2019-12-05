812d35c9810dde384e73bb08982641de41cf6a33
Adrian Cruz <drincruz@gmail.com>
2019-12-05 18:50:11 -0500

    [Day02] Program Alarm - Part 02
    Part2
     - Find the correct "noun" and "verb" that returns the end result of 19690720.

--- cpp/day02/solution.cc
+++ cpp/day02/solution.cc
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string>
 #include <sstream>
-#include <unordered_map>
 #include <vector>
 
 /**
@@ -46,17 +45,17 @@ std::vector<int> parseStr(std::string data) {
  * > 2 with the value 2.
  * My fault; next time read carefully. :(
  */
-std::vector<int> preProcess(std::vector<int> data) {
-  data[1] = 12;
-  data[2] = 2;
+std::vector<int> preProcess(std::vector<int> data, int noun, int verb) {
+  data[1] = noun;
+  data[2] = verb;
 
   return data;
 }
 
-int main() {
+void day01() {
   std::string str = readFile();
   std::vector<int> data = parseStr(str);
-  data = preProcess(data);
+  data = preProcess(data, 12, 2);
   for(std::size_t i = 0; i < data.size(); i++) {
     printf("%d\n", data[i]);
   }
@@ -84,5 +83,58 @@ int main() {
     }
   }
 
-  return 0;
+  return;
+}
+
+/**
+ * Update for Part02.
+ *
+ * Same problem, but we want to reset the memory state each time.
+ * The goal is to find the correct noun, verb combo (passed into preProcess)
+ * that will add up to 19690720.
+ */
+int day02(int noun, int verb) {
+  std::string str = readFile();
+  std::vector<int> data = parseStr(str);
+  data = preProcess(data, noun, verb);
+  std::size_t i = 0;
+
+  while (i < data.size()) {
+    int x = data[i + 1];
+    int y = data[i + 2];
+    int dest = data[i + 3];
+
+    // Addition
+    if (data[i] == 1) {
+      data[dest] = data[x] + data[y];
+      i += 4;
+    }
+    // Multiplication
+    else if (data[i] == 2) {
+      data[dest] = data[x] * data[y];
+      i += 4;
+    }
+    // Terminate
+    else if (data[i] == 99) {
+      break;
+    }
+  }
+
+  return data[0];
+}
+
+int main() {
+  int expected = 19690720;
+
+  for (int noun=0; noun <= 99; noun++) {
+    for (int verb=0; verb <=99; verb++) {
+      int ret = day02(noun, verb);
+      if (ret == expected) {
+        printf("%d == %d:\t%d %d\n", ret, expected, noun, verb);
+        return 0;
+      }
+    }
+  }
+
+  return 1;
 }
