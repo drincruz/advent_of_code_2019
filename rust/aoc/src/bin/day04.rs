90cc1b4922702b7787b103a6f4fc9d4c2d872ccf
Adrian Cruz <drincruz@gmail.com>
2019-12-29 23:38:05 -0500

    [Day04] Secure Conteiner - Part02
    Amending the repeat rule:
     - a repeat *must* be two only anything more will not be counted.

--- rust/aoc/src/bin/day04.rs
+++ rust/aoc/src/bin/day04.rs
@@ -1,20 +1,26 @@
-// Day04, Part01.
+// Day04.
 //
+// Part01
 // Given a range, find how many different passwords would be within
 // the range that meets the following criteria:
 //  - 6-digit number
 //  - value within range
 //  - two adjacent numbers are the same
-//  - Numbers from left to right are increasin
+//  - Numbers from left to right are increasing
 //    * 111123
 //    * 135679
+//
+// Part02
+// Same password rules except now, two adjacent matching digits must not
+// be part of a larger group of matching digits.
+// e.g. 111234 is not a valid repeat, but 111233 is because 33 is a proper double.
 //
 // My puzzle input is: 137683-596253
 fn main() {
     let mut valid_passwords: i32 = 0;
     for n in 137683..596253 {
         let digits: Vec<u32> = get_num_digits(n);
-        if is_valid_password(digits) {
+        if valid_format(&digits) && has_valid_repeat(&digits) {
             valid_passwords += 1;
         }
     }
@@ -29,20 +35,46 @@ fn get_num_digits(n: i32) -> Vec<u32> {
         .collect()
 }
 
-fn is_valid_password(digits: Vec<u32>) -> bool {
+fn valid_format(digits: &[u32]) -> bool {
     let mut repeat_numbers: i32 = 0;
     let mut digit_iter = digits.iter().peekable();
     while let Some(cur) = digit_iter.next() {
         if let Some(next) = digit_iter.peek() {
-            if cur > next {
-                return false;
-            } else if cur == *next {
+            if cur == *next {
                 repeat_numbers += 1;
+            } else if cur > next {
+                return false;
             }
         }
     }
-    if repeat_numbers > 0 {
-        return true;
+
+    return repeat_numbers > 0;
+}
+
+fn has_valid_repeat(digits: &[u32]) -> bool {
+    let mut repeat_count: i32 = 1;
+    let mut repeats: Vec<i32> = Vec::new();
+    let mut digit_iter = digits.iter().peekable();
+    while let Some(cur) = digit_iter.next() {
+        if let Some(next) = digit_iter.peek() {
+            if cur == *next {
+                repeat_count += 1;
+            } else {
+                if repeat_count > 1 {
+                    repeats.push(repeat_count);
+                }
+                repeat_count = 1;
+            }
+        }
+    }
+    if repeat_count > 1 {
+        repeats.push(repeat_count);
+    }
+
+    for n in &repeats {
+        if *n == 2 {
+            return true;
+        }
     }
     return false;
 }
